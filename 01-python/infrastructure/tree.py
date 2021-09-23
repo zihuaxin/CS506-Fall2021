47dc4f6162a73cae2a1069a0f100a7a0a9db7081
zihuaxin <zihuaxin@bu.edu>
2021-09-23 15:54:35 -0400

    adding tree

--- 01-python/infrastructure/tree.py
+++ 01-python/infrastructure/tree.py
@@ -1,3 +1,14 @@
 def draw_tree():
-    print("tree not found")
+    print(r"            v .   ._, |_  .,")
+    print(r'        `-._\/  .  \ /    |/_')
+    print(r'            \\  _\, y | \//')
+    print(r'        _\_.___\\, \\/ -.\||')
+    print(r'        `7-,--.`._||  / / ,')
+    print(r"        /'     `-. `./ / |/_.'")
+    print(r'                    |    |//')
+    print(r'                    |_    /')
+    print(r'                    |-   |')
+    print(r'                    |   =|')
+    print(r'                    |    |')
+    print(r'--------------------/ ,  . \--------._')
     return
